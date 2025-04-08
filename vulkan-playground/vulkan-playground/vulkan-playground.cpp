248d11f2b32aae327e71fb7be439b7fff789fe9f
Donghyeon Kim <dhygns@gmail.com>
2025-04-08 16:43:40 -0700

    Added registering queue family / more information

--- vulkan-playground/vulkan-playground/vulkan-playground.cpp
+++ vulkan-playground/vulkan-playground/vulkan-playground.cpp
@@ -9,6 +9,12 @@ VkInstance s_Instance = VK_NULL_HANDLE;
 // Vulkan Physical Device : 실제 GPU 기기를 저장한다
 VkPhysicalDevice s_PhysicalDevice = VK_NULL_HANDLE;
 
+// Queue Family Index : Graphic 작업용 Queue Family의 Index를 저장해둡니다. 
+int s_GraphicsQueueFamilyIndex = -1;
+
+// Vulkan Logical Device : 논리 Device 저장한뒤 사용
+VkDevice s_Device = VK_NULL_HANDLE;
+
 int main() {
 	if (!glfwInit()) {
 		std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -56,8 +62,14 @@ int main() {
 		VkPhysicalDeviceProperties physicalDeviceProperties;
 		vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
 		std::cout << "[" << i << "]=======[Physical Device] : " << physicalDeviceProperties.deviceName << "=======" << std::endl;
-		std::cout << "Driver Version : " << physicalDeviceProperties.driverVersion << std::endl;
-		std::cout << "API Version : " << physicalDeviceProperties.apiVersion << std::endl;
+		std::cout << "Driver Version : " <<
+			VK_VERSION_MAJOR(physicalDeviceProperties.driverVersion) << "." <<
+			VK_VERSION_MINOR(physicalDeviceProperties.driverVersion) << "." <<
+			VK_VERSION_PATCH(physicalDeviceProperties.driverVersion) << std::endl;
+		std::cout << "API Version : " <<
+			VK_VERSION_MAJOR(physicalDeviceProperties.apiVersion) << "." <<
+			VK_VERSION_MINOR(physicalDeviceProperties.apiVersion) << "." <<
+			VK_VERSION_PATCH(physicalDeviceProperties.apiVersion) << std::endl;
 		std::cout << std::endl;
 	}
 
@@ -73,8 +85,14 @@ int main() {
 			vkGetPhysicalDeviceProperties(physicalDevice, &physicalDeviceProperties);
 			std::cout << "Selected Physical Device Info [" << selectedPhysicalDeviceIndex << "]" << std::endl;
 			std::cout << "Name : " << physicalDeviceProperties.deviceName << std::endl;
-			std::cout << "Driver Version : " << physicalDeviceProperties.driverVersion << std::endl;
-			std::cout << "API Version : " << physicalDeviceProperties.apiVersion << std::endl;
+			std::cout << "Driver Version : " <<
+				VK_VERSION_MAJOR(physicalDeviceProperties.driverVersion) << "." <<
+				VK_VERSION_MINOR(physicalDeviceProperties.driverVersion) << "." <<
+				VK_VERSION_PATCH(physicalDeviceProperties.driverVersion) << std::endl;
+			std::cout << "API Version : " <<
+				VK_VERSION_MAJOR(physicalDeviceProperties.apiVersion) << "." <<
+				VK_VERSION_MINOR(physicalDeviceProperties.apiVersion) << "." <<
+				VK_VERSION_PATCH(physicalDeviceProperties.apiVersion) << std::endl;
 			std::cout << std::endl;
 			break;
 		}
@@ -83,7 +101,51 @@ int main() {
 		}
 	}
 
+	// 앞으로 쓸 Physical Device 선택 및 저장
 	s_PhysicalDevice = physicalDevices[selectedPhysicalDeviceIndex];
 
+	// Graphic 작업용 Queue Family Index 저장하기
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(s_PhysicalDevice, &queueFamilyCount, nullptr);
+
+	std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(s_PhysicalDevice, &queueFamilyCount, queueFamilies.data());
+
+	int graphicsFamilyIndex = -1;
+	for (int i = 0; i < queueFamilies.size(); i++) {
+		// VK_QUEUE_GRAPHICS_BIT 플레그를 가진 queue를 찾아 GraphicsFamilyIndex로 저장해둡니다.
+		if (queueFamilies[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+			graphicsFamilyIndex = i;
+			break;
+		}
+	}
+
+	// 만약 graphics 관련된 Family index를 찾지 못했다면, 프로그램을 종료시킵니다.
+	if (graphicsFamilyIndex == -1) {
+		std::cerr << "Failed to find a graphics queue family!" << std::endl;
+		return -1;
+	}
+
+	s_GraphicsQueueFamilyIndex = graphicsFamilyIndex;
+
+	// 논리 Device (Device라고 정의) 생성
+	float queuePriority = 1.0f;
+	VkDeviceQueueCreateInfo queueCreateInfo{};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = s_GraphicsQueueFamilyIndex;
+	queueCreateInfo.queueCount = 1;
+	queueCreateInfo.pQueuePriorities = &queuePriority;
+
+	VkDeviceCreateInfo deviceCreateInfo{};
+	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	deviceCreateInfo.queueCreateInfoCount = 1;
+	deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
+
+	if (vkCreateDevice(s_PhysicalDevice, &deviceCreateInfo, nullptr, &s_Device) != VK_SUCCESS) {
+		std::cerr << "Failed to create logical device!" << std::endl;
+		return -1;
+	}
+
+	std::cout << "Logical device created successfully!" << std::endl;
 	return 0;
 }
